d625a9062e985656182524972ce67eb3a1ae2ae4
Thiem Huynh <thiem.huynh0@gmail.com>
2023-12-03 17:46:46 -0500

    scrimba play update

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+
+import React from "react"
+import { FaReact } from "react-icons/fa";
+
+export default function Navbar() {
+    return (
+        <nav>
+            <FaReact className="logo"/>
+            <h3>ReactFacts</h3>
+            <h4>React Course - Project 1</h4>
+        </nav>
+    )
+}
