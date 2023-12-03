de755f9a1c194975ae357fdb38e166f0780fec6c
Thiem Huynh <thiem.huynh0@gmail.com>
2023-12-03 17:57:02 -0500

    layout update

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,7 @@ import React from "react"
 export default function Main() {
     return (
         <main>
-            <h1>Fun facts about React</h1>
+            <h1 className="main--title">Fun facts about React</h1>
             <ul>
                 <li>Was first released in 2013</li>
                 <li>Was originally created by Jordan Walke</li>
